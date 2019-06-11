df201514e08d5b923764bec96a8bd955551415a5
Imon Ovbude <imonovbude@gmail.com>
2019-06-11 15:42:19 -0700

    completed removeItem method functionality in context.js

--- commerce/src/context.js
+++ commerce/src/context.js
@@ -100,7 +100,24 @@ class ProductProvider extends React.Component {
   };
 
   removeItem = id => {
-    console.log("this removed the item");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      };
+    });
   };
 
   clearCart = () => {
@@ -110,6 +127,7 @@ class ProductProvider extends React.Component {
       },
       () => {
         this.setProducts();
+        this.addTotals();
       }
     );
   };
