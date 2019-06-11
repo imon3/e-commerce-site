f5727a447729273a465eef257e6bfaa6fade3940
Imon Ovbude <imonovbude@gmail.com>
2019-06-11 10:31:57 -0700

    passed methods to product.js class

--- commerce/src/components/product/Product.js
+++ commerce/src/components/product/Product.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ProductWrapper } from "../styledComponents/StyledComponents";
 import PropTypes from "prop-types";
+import { ProductConsumer } from "../../context";
 
 class Product extends React.Component {
   render() {
@@ -9,27 +10,31 @@ class Product extends React.Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => console.log("you clicked the img container")}
-          >
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top" />
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart ? true : false}
-              onClick={() => console.log("added to the cart")}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  in cart
-                </p>
-              ) : (
-                <i className="fas fa-cart-plus" />
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {value => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail(id)}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => value.addToCart(id)}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      in cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center">{title}</p>
             <h5 className="text-blue font-italic mb-0">
--- commerce/src/context.js
+++ commerce/src/context.js
@@ -30,12 +30,20 @@ class ProductProvider extends React.Component {
     });
   }
 
-  handleDetail = () => {
-    console.log("hello from detail");
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
 
   addToCart = id => {
-    console.log(`hello from add to cart.id is ${id}`);
+    let tempProducts = [...this.state.products];
+  };
+
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
   };
 
   render() {
