7afb0f70027b8bc8dffb7eea3c92360355db2b6d
Imon Ovbude <imonovbude@gmail.com>
2019-06-13 17:24:11 -0700

    created env key

--- commerce/src/components/paypal/PayPal.js
+++ commerce/src/components/paypal/PayPal.js
@@ -6,6 +6,8 @@ export default class PayPalButton extends React.Component {
     const onSuccess = payment => {
       // Congratulation, it came here means everything's fine!
       console.log("The payment was succeeded!", payment);
+      this.props.clearCart();
+      this.props.history.push("/");
       // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
@@ -24,12 +26,11 @@ export default class PayPalButton extends React.Component {
 
     let env = "sandbox"; // you can set here to 'production' for production
     let currency = "USD"; // or you can set this value from your props or state
-    let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    let total = this.props.total; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox:
-        "YOUR-SANDBOXAX3MdThcqleUC-MpSYUxJX8E1PdJULBYJHuiyNhTlay2PB-AmUrQS3dzmFvRJfNafPh9w3gd0NWMn6lR-APP-ID",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
